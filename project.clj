f4fd493a2885daa73f2fbd86180a22821ab36b49
Constantine Vetoshev <gepardcv@gmail.com>
2010-05-29 12:03:37 -0400

    Added leiningen swank-clojure dependency.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject dgraph "1.1.0"
   :description "A dependency graph library for Clojure."
-  :dependencies [[org.clojure/clojure "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
